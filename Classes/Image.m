52d0875cb67b66009ea3673bf1183402975d9d6d
zak <uphgreat@gmail.com>
2014-10-23 16:38:06 -0400

    Added plotting col and row sums to Image.m
    Also included new values for ROI and the new back_ROI attribute

--- Classes/Image.m
+++ Classes/Image.m
@@ -6,6 +6,7 @@ classdef Image < dynamicprops
     properties
         %Region of Interest
         ROI %will have xmin xmax, ymin, ymax attributes
+        back_ROI %Region to use for scaling background signal (coordinates relative to ROI)
         
         %Actual Image data
         raw_image_filename=''; %image with signal over background
@@ -24,14 +25,10 @@ classdef Image < dynamicprops
             %   have the name image_name_back.ascii
             if nargin > 0
                 %Determine file names
-                self.raw_image_filename=[image_name,'.ascii'];
-                self.back_image_filename=[image_name,'_back.ascii'];
+                self.set_file_names(image_name);
                 
                 %Set default values for initial parameters
-                self.ROI.xmin=50;
-                self.ROI.xmax=250;
-                self.ROI.ymin=100;
-                self.ROI.ymax=250;
+                self.set_default_values();
                 
                 %Load data
                 self.load_raw_image();
@@ -40,10 +37,37 @@ classdef Image < dynamicprops
                 %Remove background
                 self.remove_background();
                 
-                %Free up memmory
-                self.unload_raw_image();
-                self.unload_back_image();
+                %Free up memmory %Change of plans, let's keep this data
+                %around for now for debugging purposes.
+                %                 self.unload_raw_image();
+                %                 self.unload_back_image();
             end
+        end
+        
+        function [] = set_file_names(self,image_name)
+            %Determines the file names for raw_data and back_data
+            self.raw_image_filename=[image_name,'.ascii'];
+            self.back_image_filename=[image_name,'_back.ascii'];
+        end
+        
+        function [] = set_default_values(self)
+            %This function sets default values when an Image instance is
+            %initialized.
+            
+            %Set Region of Interest
+            self.ROI.xmin=71; %51;
+            self.ROI.xmax=260; %250;
+            self.ROI.ymin=101;
+            self.ROI.ymax=270;
+            
+            %Set background Region of Interest
+            %  Coordinates are relative to the ROI, not the original image.
+            %  This region is used to figure out how to scale the
+            %  background before subtracting it from raw_data.
+            self.back_ROI.xmin=131;
+            self.back_ROI.xmax=180;
+            self.back_ROI.ymin=110;
+            self.back_ROI.ymax=140;
         end
         
         function [] = load_raw_image(self)
@@ -85,7 +109,7 @@ classdef Image < dynamicprops
             %Adds a new attribute called name and assigns it to be value
             %   name should be a string
             addprop(self,name);
-            self.(name)=value;          
+            self.(name)=value;
         end
         
         function [] = set_metadata(self,name,value)
@@ -109,12 +133,64 @@ classdef Image < dynamicprops
                 value=self.(name);
             end
         end
+        
+        function [] = compare_row_sums(self)
+            %Plots the row sums of raw_image, back_image, and image
+            sum_direction=2;
+            self.compare_sums_helper(sum_direction);
+        end
+        
+        function [] = compare_col_sums(self)
+            %Plots the row sums of raw_image, back_image, and image
+            sum_direction=1;
+            self.compare_sums_helper(sum_direction);
+        end
     end
     
-    methods (Static)    
+    methods (Hidden)
+        
+        function [] = compare_sums_helper(self,sum_direction)
+            %Performs the plotting for compare_row_sums and
+            %compare_col_sums
+            
+            %Plot raw_image and back_image
+            figure('WindowStyle','docked');
+            title('Camera Data');
+            hold on;
+            Image.plot_array_sums(self.raw_image,sum_direction);
+            Image.plot_array_sums(self.back_image,sum_direction,'r');
+            legend('raw','back');
+            hold off;
+            
+            %Plot background-removed data
+            figure('WindowStyle','docked');
+            Image.plot_array_sums(self.image,sum_direction);
+            scaling=(sum(self.raw_image,sum_direction)./sum(self.back_image,sum_direction));
+            title('Background Removed');
+            
+            %Plot the scaling (raw/background)
+            figure('WindowStyle','docked');
+            plot(scaling);
+            title('(raw\_data row sum)/(back\_data row sum)');
+        end
+        
+    end
+    
+    methods (Static)
         function [image_array] = load_image_data(filename)
             %Returns an array containing the data from filename
             image_array=importdata(filename);
+        end
+        
+        function [] = plot_array_sums(array,sum_direction,varargin)
+            %Sums up the values in each row or column and plots the results
+            %  Set sum_direction=2 to sum up each row, or sum_direction=1
+            %  to sum up each column.
+            %  Additional arguments may be specified, which will be passed
+            %  along to the plot function so that you can specify
+            %  linestyle, color, etc.
+            rows=sum(array,sum_direction);
+            plot(rows,varargin{:});
         end
     end
     
