ea965b764b5b389b75e0d33099442f7e74ed0f55
waveguide4 <zakven@mit.edu>
2018-06-09 21:44:12 -0400

    Commiting changed ROI etc. before working on using metadata

--- Camera_Control/Main_PCO_Pixelfly_USB_flu.m
+++ Camera_Control/Main_PCO_Pixelfly_USB_flu.m
@@ -50,7 +50,9 @@ function Main_PCO_Pixelfly_USB_flu(run_config,image_instance_data)
 % row_min=50; row_max=350; col_min=1; col_max=171; %Region that may have atoms
 % row_min=35; row_max=135; col_min=1; col_max=301; %Region that may have atoms
 % row_min=10; row_max=70; col_min=1; col_max=800; %Region that may have atoms
-row_min=35; row_max=135; col_min=1; col_max=171; %Region that may have atoms
+% row_min=35; row_max=135; col_min=1; col_max=171; %Region that may have atoms
+% row_min=1; row_max=171; col_min=1; col_max=171; %Region that may have atoms
+row_min=20; row_max=120; col_min=1; col_max=301; %Region that may have atoms
 
 %Set range for colobar scale of atom OD plot
 OD_colorbar_range=[-0.1,0.5]*1.2;
@@ -60,10 +62,10 @@ OD_colorbar_range=[-0.1,0.5]*1.2;
 %Set region of interest for analysis [row_min,row_max;col_min,col_max]
 %(Note semicolon between row and columns indices)
 % analysis_ROI=[1,405;298,468];
-% analysis_ROI=[248,418;233,533];
+analysis_ROI=[235,405;230,530];
 % analysis_ROI=[260,340;1,800];
 % analysis_ROI=[215,385;295,465];
-analysis_ROI=[215,385;345,515];
+% analysis_ROI=[215,385;345,515];
 
 %unpack data from argument object
 savingname=run_config.namefile;
