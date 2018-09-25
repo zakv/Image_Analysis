eb61405f486af885d30d3025bccdd60b8370845d
waveguide4 <zakven@mit.edu>
2018-09-25 15:48:58 -0400

    Increased number of background images used

--- Functions/quick_back_removal_eig.m
+++ Functions/quick_back_removal_eig.m
@@ -24,7 +24,7 @@ function [ OD ] = quick_back_removal_eig( saving_path, image_in, row_min, row_ma
 %   temppart will be a 2D array giving the OD of the cloud (after the
 %   background has been removed)
 
-max_input_backs=10; %50; %Maximum number of background images to use
+max_input_backs=20; %50; %Maximum number of background images to use
 
 %First get the most recent files, up to max_input_backs of them
 file_list=get_recent_back_list_fast( saving_path, max_input_backs );
